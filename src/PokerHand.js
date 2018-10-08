238d852959824193923d50ce25158abbe5d96254
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2018-10-08 22:55:06 +0100

    added functions to calculate royal flush and straight Flush

--- src/PokerHand.js
+++ src/PokerHand.js
@@ -22,13 +22,13 @@ class PokerHand {
   constructor(hand) {
     this.hand = hand;
     this.formattedHand = this.generateHand();
-    this.handScore = this.calculateHandScore(this.hand);
+    this.handScore = this.calculateHandScore();
   }
 
   generateHand() {
     return this.hand.split(' ').map((x) => {
       let newVal;
-      switch (x.split('')[0]) {
+      switch (x.slice(0, -1)) {
         case 'A':
           newVal = 14;
           break;
@@ -42,18 +42,49 @@ class PokerHand {
           newVal = 13;
           break;
         default:
-          newVal = parseInt(x.split('')[0]);
+          newVal = parseInt(x.slice(0, -1));
           break;
       }
-      return { value: newVal, suit: x.split('')[1] }
+      return { value: newVal, suit: x[x.length - 1] }
     });
   }
 
   calculateHandScore() {
+    if (this.isRoyalFlush()){
+      return {rank: 10, score: null};
+    } else if (this.isStraightFlush()){
+      return {rank: 9, score: this.isStraightFlush()}
+    } else {
+      return 'high card';
+    }
     
-    return 'score';
   };
 
+  isRoyalFlush(){
+    const initialValue = 0;
+    return (
+      this.formattedHand.every((val, i, arr) => val.suit === arr[0].suit) && 
+      this.formattedHand.reduce(function (accumulator, currentValue) {
+          return accumulator + currentValue.value;
+      },initialValue) === 60
+    )
+  };
+
+  isStraightFlush() {
+    const initialValue = 0;
+    const sortedHand = this.formattedHand.map((x) => {return x.value}).sort();
+    const differenceBetweenValues = sortedHand[sortedHand.length -1] - sortedHand[0];
+    if(
+      this.formattedHand.every((val, i, arr) => val.suit === arr[0].suit) && 
+      differenceBetweenValues === 4
+    ){
+      return this.formattedHand.reduce(function (accumulator, currentValue) {
+        return accumulator + currentValue.value;
+    },initialValue);
+    } else{
+      return false;
+    }
+  }
 
   compareWith(handTwo) {
     // console.log(this.handScore, handTwo.handScore)
@@ -69,8 +100,9 @@ const Result = {
 
 const handOne = new PokerHand('AC 4S 5S 8C AH');
 const handTwo = new PokerHand('4S 5S 8C AS AD');
+const handThree = new PokerHand('AC KC QC JC 10C');
 
-console.log(handOne, 'divide', handTwo);
+console.log(handOne, 'divide', handTwo, 'divide', handThree);
 
 handOne.compareWith(handTwo);
 
