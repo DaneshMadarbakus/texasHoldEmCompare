38bd59066a5b128afd00a9ff8a10fbd4f186b645
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2018-10-09 01:24:39 +0100

    refactored compare function, added more scoring systems

--- src/PokerHand.js
+++ src/PokerHand.js
@@ -22,7 +22,7 @@ class PokerHand {
   constructor(hand) {
     this.hand = hand;
     this.formattedHand = this.generateHand();
-    this.sortedValues = this.formattedHand.map((x) => {return x.value}).sort((a, b)=>{return a-b});
+    this.sortedValues = this.formattedHand.map((x) => { return x.value }).sort((a, b) => { return a - b });
     this.handScore = this.calculateHandScore();
   }
 
@@ -51,68 +51,88 @@ class PokerHand {
   }
 
   calculateHandScore() {
-    const scoringSystem = [this.isRoyalFlush(), this.isStraightFlush(), this.isFourOfKind(), this.isFullHouse()];
-    
-    for (var i = 0; i < scoringSystem.length; i++){
-      if (scoringSystem[i]){
-        return scoringSystem[i];
+    const scoringSystem = [this.isRoyalFlush(), this.isStraightFlush(), this.isFourOfKind(), this.isFullHouse(), this.isFlush()];
 
+    for (let i = 0; i < scoringSystem.length; i++) {
+      if (scoringSystem[i]) {
+        return scoringSystem[i]; 
       }
     }
-
   };
 
-  isFullHouse (){
+  isFlush() {
+    if (this.formattedHand.every((val, i, arr) => val.suit === arr[0].suit)) {
+      return { rank: 6, score: this.sortedValues.reverse() };
+    } else return false;
+  }
+
+  isFullHouse() {
     let valueOfThree;
-    if(this.sortedValues[0] === this.sortedValues[2] && this.sortedValues[3] === this.sortedValues[4] || this.sortedValues[0] === this.sortedValues[1] && this.sortedValues[2] === this.sortedValues[4]){
-      if (this.sortedValues[0] === this.sortedValues[2]){
-        valueOfThree = this.sortedValues[0] + this.sortedValues[2];
-      } else if (valueOfThree = this.sortedValues[2] === this.sortedValues[4]){
-        valueOfThree = this.sortedValues[2] + this.sortedValues[4];
+    if (this.sortedValues[0] === this.sortedValues[2] && this.sortedValues[3] === this.sortedValues[4] || this.sortedValues[0] === this.sortedValues[1] && this.sortedValues[2] === this.sortedValues[4]) {
+      if (this.sortedValues[0] === this.sortedValues[2]) {
+        valueOfThree = this.sortedValues[0];
+      } else if (valueOfThree = this.sortedValues[2] === this.sortedValues[4]) {
+        valueOfThree = this.sortedValues[4];
       }
-      return {rank: 7, score: valueOfThree};
+      return { rank: 7, score: [valueOfThree] };
     } else return false;
   }
 
   isFourOfKind() {
-    if (this.sortedValues.slice(0, 4)[0] === this.sortedValues.slice(0, 4)[3]){
-      return {rank: 8, score: this.sortedValues.slice(0, 4).reduce((accumulator, currentValue) => {
-        return accumulator + currentValue;
-      })};
+    if (this.sortedValues[0] === this.sortedValues[3] || this.sortedValues[1] === this.sortedValues[4] ) {
+      return {
+        rank: 8, score: [this.sortedValues[2]]
+      }
     } else return false;
   }
 
   isStraightFlush() {
     const initialValue = 0;
-    const differenceBetweenValues = this.sortedValues[this.sortedValues.length -1] - this.sortedValues[0];
-    if(
-      this.formattedHand.every((val, i, arr) => val.suit === arr[0].suit) && 
+    const differenceBetweenValues = this.sortedValues[this.sortedValues.length - 1] - this.sortedValues[0];
+    if (
+      this.formattedHand.every((val, i, arr) => val.suit === arr[0].suit) &&
       differenceBetweenValues === 4
-    ){
-      return {rank: 9, score: this.formattedHand.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue.value;
-    },initialValue)}
-    } else{
-      return false;
-    }
+    ) {
+      return {
+        rank: 9, score: [this.formattedHand.reduce((accumulator, currentValue) => {
+          return accumulator + currentValue.value;
+        }, initialValue)]
+      }
+    } else return false;
   }
 
-  isRoyalFlush(){
+  isRoyalFlush() {
     const initialValue = 0;
     if (
-      this.formattedHand.every((val, i, arr) => val.suit === arr[0].suit) && 
+      this.formattedHand.every((val, i, arr) => val.suit === arr[0].suit) &&
       this.formattedHand.reduce(function (accumulator, currentValue) {
-          return accumulator + currentValue.value;
-      },initialValue) === 60
+        return accumulator + currentValue.value;
+      }, initialValue) === 60
     ) {
-      return {rank: 10, score: 100};
-    } else {
-      return false;
-    }
+      return { rank: 10, score: [100] };
+    } else return false;
   };
 
   compareWith(handTwo) {
-    return Result.TIE;
+    function compareScore(x, y) {
+      for (let n = 0; n < x.length; n++) {
+        if (x[n] > y[n]) {
+          return Result.WIN;
+        } else if (x[n] < y[n]) {
+          return Result.LOSS;
+        }
+      }
+    }
+
+    if (this.handScore === handTwo.handScore) {
+      return Result.TIE;
+    } else if (this.handScore.rank > handTwo.handScore.rank) {
+      return Result.WIN;
+    } else if (this.handScore.rank < handTwo.handScore.rank) {
+      return Result.LOSS;
+    } else if (this.handScore.rank === handTwo.handScore.rank) {
+      return compareScore(this.handScore.score, handTwo.handScore.score);
+    }
   }
 }
 
@@ -124,9 +144,8 @@ const Result = {
 
 const handOne = new PokerHand('AC 4S 5S 8C AH');
 const handTwo = new PokerHand('4S 5S 8C AS AD');
-const handThree = new PokerHand('4A 4H 4C 10S 10C');
+const handThree = new PokerHand('7A 7H 7C 7S 3C');
 const handFour = new PokerHand('5A 5H 5C 5S 6C');
 
-console.log(handOne, 'divide', handTwo, 'divide', handThree, 'divide', handFour);
-
-handOne.compareWith(handTwo);
+console.log(handThree);
+console.log(handFour.compareWith(handThree));
